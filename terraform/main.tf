af70ddc6d9153634026b7e1c0f22efa74637d989
anasdweik <a.dweik.99@gmail.com>
2025-02-04 18:02:47 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,3 +38,6 @@ output "ec2_public_ip" {
   description = "The public IP of the EC2 instance"
   value       = aws_instance.web_server.public_ip
 }
+output "ec2_public_ip" {
+  value = aws_instance.web_server.public_ip
+}
